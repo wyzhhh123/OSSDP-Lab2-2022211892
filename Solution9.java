5ddbc4f241ec2cf017fb64fcc26600307f94591c
wyzhhh123 <wyzhhh123@gmail.com>
2024-10-21 11:28:43 +0800

    fixed errors and added tests

--- Solution9.java
+++ Solution9.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @description:
@@ -42,16 +43,20 @@ class Solution9 {
         int[] fa = new int[n + 1];
         Arrays.fill(fa, -1);
         List<Integer>[] g = new List[n + 1];
-        for (int i = 0; i < n; ++i) {
-            g[i] = new ArrayList<Integer>();
+        for (int i = 1; i <= n; ++i) {
+            g[i] = new ArrayList<>();
         }
-        for (int[] p : dislikes)
+        for (int[] p : dislikes){
             g[p[0]].add(p[1]);
             g[p[1]].add(p[0]);
+        }
         for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j < g[i].size(); ++j) {
+            if (g[i].isEmpty()){
+                continue;
+            }
+            for (int j = 1; j < g[i].size(); ++j) {
                 unit(g[i].get(0), g[i].get(j), fa);
-                if (isconnect(i, g[i].get(j), fa)) {
+                if (isConnected(i, g[i].get(j), fa)) {
                     return false;
                 }
             }
@@ -62,25 +67,44 @@ class Solution9 {
     public void unit(int x, int y, int[] fa) {
         x = findFa(x, fa);
         y = findFa(y, fa);
-        if (x == y) {
-            return ;
+        if (x != y) {
+            fa[y] = x ;
         }
-        if (fa[x] <= fa[y]) {
-            int temp = x;
-            x = y;
-            y = temp;
-        }
-        fa[x] += fa[y];
-        fa[y] = x;
+
     }
 
-    public boolean isconnect(int x, int y, int[] fa) {
-        x = findFa(x, fa);
-        y = findFa(y, fa);
-        return x == y;
+    public boolean isConnected(int x, int y, int[] fa) {
+        return findFa(x, fa) == findFa(y, fa);
     }
 
     public int findFa(int x, int[] fa) {
-        return fa[x] > 0 ? x : (fa[x] = findFa(fa[x], fa));
+       if (fa[x] < 0) {
+            return x;
+       }
+        return fa[x] = findFa(fa[x], fa);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Solution9 solution = new Solution9();
+        System.out.println("请输入人数 n：");
+        int n = scanner.nextInt();
+        scanner.nextLine(); 
+        List<int[]> dislikesList = new ArrayList<>();
+        System.out.println("请输入每对不喜欢的人的编号 (ai bi)：");
+        while (true) {
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) {
+                break;  // 空行结束输入
+            }
+            String[] parts = line.split("\\s+");  // 以空格分隔
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[1]);
+            dislikesList.add(new int[]{a, b});
+        }
+        int[][] dislikes = dislikesList.toArray(new int[dislikesList.size()][]);
+        boolean result = solution.possibleBipartition(n, dislikes);
+        System.out.println("是否可以分成两组：" + result);
+
+        scanner.close();
     }
 }
